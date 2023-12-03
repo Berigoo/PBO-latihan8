d395457ebf29d2bfcf0313e8f0718e6ede82c1ca
Berigoo <muhamadrizqifajri2@gmail.com>
2023-12-03 20:47:47 +0700

    Done - 5 hours of my work was wasted for 5 minutes of work ;)

--- src/com/calc/Calc.java
+++ src/com/calc/Calc.java
@@ -15,78 +15,104 @@ public class Calc {
                 case 42: // *
                     val += " *";
                     opr = 42;
-                    outputField.setText(Double.toString(left) + " " + (char)opr);
+                    outputField.setText(left + " " + (char)opr + " " + right);
                     isLeft = false;
                     break;
                 case 47: // /
                     val += " /";
                     opr = 47;
-                    outputField.setText(Double.toString(left) + " " + (char)opr);
+                    outputField.setText(left + " " + (char)opr + " " + right);
                     isLeft = false;
                     break;
                 case 43: // +
                     val += " +";
                     opr = 43;
-                    outputField.setText(Double.toString(left) + " " + (char)opr);
+                    outputField.setText(left + " " + (char)opr + " " + right);
                     isLeft = false;
                     break;
                 case 45: // -
                     val += " -";
                     opr = 45;
-                    outputField.setText(Double.toString(left) + " " + (char)opr);
+                    outputField.setText(left + " " + (char)opr + " " + right);
                     isLeft = false;
                     break;
                 case 61: // =
+                    if(left.isEmpty()) left = "0";
+                    if(right.isEmpty()) right = "0";
+                    double _left = Double.parseDouble(left);
+                    double _right = Double.parseDouble(right);
                     if(opr == 42){
-                        left = left * right;
+                        left = Double.toString( _left * _right);
                     } else if (opr == 47) {
-                        if(right == 0){
+                        if(right.isEmpty() || right.equals("0")){
                             outputField.setText("buh.");
                             isLeft = true;
-                            left = 0;
+                            left = "";
                             break;
                         }else {
-                            left = left / right;
+                            left = Double.toString( _left / _right);
                         }
                     } else if (opr == 43) {
-                        left = left + right;
+                        left = Double.toString( _left + _right);
                     } else if (opr == 45) {
-                        left = left - right;
+                        left = Double.toString( _left - _right);
                     } else if (opr == 37) {
-                        left = left % right;
+                        left = Double.toString( _left % _right);
                     }
-                    outputField.setText(Double.toString(left));
-                    right = 0;
-                    isLeft = true;
+                    outputField.setText(left);
+                    right = "";
+                    isLeft = false;
                     break;
                 case 37: // %
                     opr = 37;
-                    outputField.setText(Double.toString(left) + " " + (char)opr);
+                    outputField.setText(left + " " + (char)opr + " " + right);
                     isLeft = false;
                     break;
                 case 66: // B
+                    if(isLeft){
+                        if(left.length()>1)left = left.substring(0, left.length()-1);
+                        else left = "0";
+                        outputField.setText(left);
+                    }else {
+                        if(right.length()>1)right = right.substring(0, right.length() - 1);
+                        else right = "0";
+                        outputField.setText(left + " " + (char)opr + " " + right);
+                    }
                     break;
                 case 69: // E
+                    if(isLeft){
+                        left = "0";
+                        outputField.setText(left);
+                    } else{
+                        right = "0";
+                        outputField.setText(left + " " + (char)opr + " " + right);
+                    }
                     break;
                 case 67: // C
+                    outputField.setText("");
+                    left = "";
+                    right = "";
+                    isLeft = true;
                     break;
                 case 46: // .
+                    if(isLeft)left = left.concat(".");
+                    else right = right.concat(".");
                     break;
                 default:
                     if(isLeft){
-                        if(left == 0)left = btn.getInteger();
-                        else left = left * Math.pow(10, 1) + btn.getInteger();
-                        outputField.setText(Double.toString(left));
+                        if(left.length() == 1 && left.compareTo("0") == 0) left = btn.getName();
+                        else left = left.concat(btn.getName());
+                        outputField.setText(left);
                     }else {
-                        if(right == 0)right = btn.getInteger();
-                        else right = right * Math.pow(10, 1) + btn.getInteger();
-                        outputField.setText((Double.toString(left) + " " + (char)opr + " " + right));
+                        if(right.length() == 1 && right.compareTo("0") == 0) right = btn.getName();
+                        else right = right.concat(btn.getName());
+                        outputField.setText(left + " " + (char)opr + " " + right);
                     }
                     break;
             }
         }
     }
-    private double left=0, right=0, res=0;
+    private String left="", right="", res="";
     int lloop = 0;
     private boolean isLeft = true;
     int opr = 43;
@@ -101,4 +127,5 @@ public class Calc {
     public _actionListener getActionListener () {
         return actionListener;
     }
+
 }
